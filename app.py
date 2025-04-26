96f976d47d95e8a105096855c42dfdcafc5e935d
Yousif Abdeljawad <yousifjawad68@gmail.com>
2025-04-25 23:28:55 -0400

    editing....

--- app.py
+++ app.py
@@ -5,6 +5,19 @@ app = Flask(__name__,
             static_url_path='/static',
             )
 
+DB_FILE = "login.db"
+    
+sqliteConnection = sqlite3.connect(DB_FILE)
+cursor = sqliteConnection.cursor()
+cursor.execute('''CREATE TABLE IF NOT EXISTS users(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT UNIQUE NOT NULL,
+            password TEXT NOT NULL)
+''')
+sqliteConnection.commit()
+sqliteConnection.close()
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -14,6 +27,17 @@ def index():
 def about():
     return render_template('about.html')
 
+
+@app.route('/login', methods =['GET','POST'])
+def login():
+    conn = sqlite3.connect('login.db')
+    c = conn.cursor
+    error = None
+    if request.method == 'POST':
+        username = request.form['username']
+
+    return render_template('login.html')
+    
 
 
 
