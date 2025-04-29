f4a0e445e9e6771e3f1712f9df9338f7fc38c125
Yousif Abdeljawad <yousifjawad68@gmail.com>
2025-04-29 00:36:08 -0400

    added flash alerts upon login and registration. Dashboard page created, logout functionality added Added further authentication to ensure unique emails

--- app.py
+++ app.py
@@ -5,40 +5,52 @@ from flask_login import LoginManager, UserMixin, login_user, login_required, log
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, validators
 from wtforms.validators import  Length, EqualTo, Email, ValidationError, InputRequired, ValidationError
-from werkzeug.security import generate_password_hash, check_password_hash
 from flask_bcrypt import Bcrypt
-import os
-if os.path.exists('database.db'):
-    os.remove('database.db')
+
 app = Flask(__name__,
             template_folder='templates',
             static_url_path='/static',
             )
+
+# pass encryption method
 bcrypt = Bcrypt(app)
 
-my_email = "programnet2@gmail.com"
-app_password = "zfch tqsd edxr hfqw"
+# Using smtplib to send emails to new users
 
-app.config['SECRET_KEY'] = 'your_secret_key'
+with open("instance/app_pw.txt", "r") as file:
+    app_password = file.read()
+my_email = "programnet2@gmail.com"
+app_password = app_password.strip()
+
+with open("instance/secret_key.txt", "r") as file:
+    secret_key = file.read()
+
+# configuration for the app database
+app.config['SECRET_KEY'] =secret_key.strip()
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+
+# Flask-Login setup
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
+
+
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-
+# User model
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(30), unique=True, nullable=False)
     password = db.Column(db.String(70), nullable=False)
     first_name = db.Column(db.String(35), nullable=False)
 
+# Registration and Login forms
 class RegistrationForm(FlaskForm):
     first_name = StringField('Name', validators=[InputRequired(), Length(min=2, max=35)], render_kw={'placeholder': 'Name'})
     email = StringField('Email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)], render_kw={'placeholder': 'Email'})
@@ -51,10 +63,9 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[InputRequired(), Length(min=6, max=35)], render_kw={'placeholder': 'Password'})
     submit = SubmitField('Login')
 
-def validate_email(self, email):
-    user = User.query.filter_by(email=email.data).first()
-    if user:
-        raise ValidationError('Email already registered. Please use a different email.')    
+# Checks to see if the email has already been registered
+
+
 
 def welcome_email(to, subject, body):
     try:
@@ -78,17 +89,31 @@ def about():
     return render_template('about.html')
 
 @app.route('/register', methods=['GET', 'POST'])
-def signup():
+def register():
     form = RegistrationForm()
+    password = form.password.data
+    confirm_password = form.confirm_password.data
+    email = form.email.data
+    
+    existing_user = User.query.filter_by(email=form.email.data).first()
 
-    if form.validate_on_submit():
-        hashed_password = generate_password_hash(form.password.data)
-        new_user = User(email=form.email.data, password=hashed_password, first_name=form.first_name.data)
-        db.session.add(new_user)
-        welcome_email(form.email.data, "Welcome to our platform", "Thank you for signing up!")
-        db.session.add(new_user)
-        db.session.commit()
+    if existing_user:
+        flash('Email already registered. Please log in.', 'danger')
         return redirect(url_for('login'))
+    if form.validate_on_submit():
+            if password != confirm_password:
+                flash('Passwords do not match', 'danger')
+                return redirect(url_for('register'))
+            else:
+                hashed_password = bcrypt.generate_password_hash(form.password.data)
+                new_user = User(email=form.email.data, password=hashed_password, first_name=form.first_name.data)
+                db.session.add(new_user)
+                welcome_email(form.email.data, "Welcome to our platform", "Thank you for signing up!")
+                db.session.add(new_user)
+                db.session.commit()
+                flash('Registration successful! Please log in.', 'success')
+                return redirect(url_for('login'))
+   
     return render_template('register.html', form=form)
 
 @app.route('/login', methods =['GET','POST'])
@@ -96,12 +121,14 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
-        if user and check_password_hash(user.password, form.password.data):
+        # checking is password with stored hash
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user)
             flash('Login successful!', 'success')
             return redirect(url_for('dashboard'))
         else:
             flash('Invalid email or password', 'danger')
+    
 
 
     redirect(url_for('dashboard'))
@@ -111,9 +138,18 @@ def login():
 @app.route('/dashboard', methods=['GET', 'POST'])
 @login_required
 def dashboard():
+        # check if the user is already logged in
+        if not current_user.is_authenticated:
+            flash('Please log in to access the dashboard.', 'danger')
+            return redirect(url_for('login'))
         return render_template('dashboard.html', name=current_user.first_name)
    
-
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.', 'success')
+    return redirect(url_for('login'))
 
 
 
